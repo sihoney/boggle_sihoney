5ef8296e757410f1bfc4dc32b170b06d11d67fc3
이영림 <rm1113@naver.com>
2022-02-22 14:25:09 +0900

    4조 프로젝트

---
+++ src/main/java/com/javaex/controller/test.java
@@ -0,0 +1,16 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test {
+	
+	@RequestMapping("/test")
+	public String test() {
+		System.out.println("test");
+		
+		return "test";
+	}
+
+}
