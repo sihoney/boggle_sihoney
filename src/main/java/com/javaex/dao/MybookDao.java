1bbc6dca4748425499a0d5dad2a1d7b4ac799113
이영림 <rm1113@naver.com>
2022-03-19 20:41:23 +0900

    0rim.

--- src/main/java/com/javaex/dao/MybookDao.java
+++ src/main/java/com/javaex/dao/MybookDao.java
@@ -24,6 +24,53 @@ public class MybookDao {
 		return mbList;
 	}
 	
+	public List<MybookVo> getPopular(int userNo){
+		System.out.println("MybookDao.getPopular()");
+		
+		List<MybookVo> mbList = sqlSession.selectList("mybook.selectpopular", userNo);
+		System.out.println(mbList.toString());
+		
+		return mbList;
+	}
+	
+	public int checklike(MybookVo checklike){
+		//System.out.println("MybookDao.checklike()");
+		
+		List<MybookVo> likecount = sqlSession.selectList("mybook.checklike", checklike);
+		int count = likecount.size();
+		
+		//System.out.println(count+"건 조회됨");
+				
+		return count;
+	}
+	
+	public MybookVo checklikecnt(MybookVo checklike){
+		//System.out.println("MybookDao.checklike()");
+		
+		//좋아요 몇개인지 담아서 보냄
+		MybookVo likecnt = sqlSession.selectOne("mybook.checklikecnt", checklike);
+		
+		System.out.println("Dao좋아요갯수" +likecnt);
+		
+		return likecnt;
+	}
+	
+	public void like(MybookVo checklike) {
+		System.out.println("MybookDao.like()");
+		
+		int count = sqlSession.insert("mybook.like", checklike);
+		System.out.println(count+"건을 좋아요하였습니다.");
+	}
+	
+	
+	
+	
+	public void dislike(MybookVo checklike) {
+		System.out.println("MybookDao.dislike()");
+		
+		int count = sqlSession.delete("mybook.dislike", checklike);
+		System.out.println(count+"건을 좋아요 취소하였습니다.");
+	}
 	
 	
 }
--- src/main/java/com/javaex/service/MybookService.java
+++ src/main/java/com/javaex/service/MybookService.java
@@ -24,6 +24,61 @@ public class MybookService {
 		return mbList;
 	}
 	
+	//유저번호 입력시 해당유저 서평리스트 출력해주는 메소드(최신순)
+	public List<MybookVo> popularlist(int userNo){
+		System.out.println("mybookService.popularlist()");
+		
+		List<MybookVo> mbList = mybookDao.getPopular(userNo);
+		
+		return mbList;
+	}
 	
+	
+	//좋아요 여부 확인
+	public int likeok(MybookVo checklike) {
+		//System.out.println("mybookService.likeok()");
 
+		int count = mybookDao.checklike(checklike);
+
+		return count;
+	}
+	
+	
+	//좋아요 확인, 좋아요 몇개인지 확인하는 메소드
+	public MybookVo likecnt(MybookVo checklike) {
+		//System.out.println("mybookService.likeok()");
+		
+		//좋아요 여부 확인
+		int count = mybookDao.checklike(checklike);
+		
+		//좋아요 몇개인지 확인
+		MybookVo likeok = mybookDao.checklikecnt(checklike);
+		
+		//두개를 Vo에 넣기
+		likeok.setLikecheck(count);		
+		
+		System.out.println(likeok);
+		
+		return likeok;
+	}
+	
+	//좋아요를 하는 메소드(review_user에 인서트)
+	public int like(MybookVo checklike) {
+		System.out.println("mybookService.like()");
+		
+		//현재+1
+		mybookDao.like(checklike);
+		
+		//실제 값가져오기
+		return 5;
+	}
+	
+	//좋아요 취소하는 메소드(review_user에서 삭제)
+	public void dislike(MybookVo checklike) {
+		System.out.println("mybookService.dislike()");
+		
+		mybookDao.dislike(checklike);
+	}
+	
+	
 }
